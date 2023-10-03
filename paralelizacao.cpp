c108dbc663a822d48609f8d74394065c933c4eab
gabri190 <gabriel_avioes@hotmail.com>
2023-10-03 11:30:21 -0300

    delete uma paralelizcao

--- paralelizacao.cpp
+++ paralelizacao.cpp
@@ -3,3 +3,85 @@
 // biblioteca MPI (Message Passing Interface). Cada processador será responsável por encontrar cliques em uma parte do grafo, e 
 // os resultados serão combinados no final para encontrar todas as cliques no grafo.
 //solucao aqui 
+
+// Paralelização e Distribuição do Processamento com MPI
+// Nesta implementação, você deve dividir o problema em várias partes e distribuí-las para diferentes processadores usando a 
+// biblioteca MPI (Message Passing Interface). Cada processador será responsável por encontrar cliques em uma parte do grafo, e 
+// os resultados serão combinados no final para encontrar todas as cliques no grafo.
+//solucao aqui 
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <omp.h>
+
+#define MAX_VERTICES 1000
+
+int graph[MAX_VERTICES][MAX_VERTICES]; // Matriz de adjacência
+int numVertices; // Número de vértices no grafo
+int maxCliqueSize; // Tamanho máximo da clique
+int clique[MAX_VERTICES]; // Conjunto de vértices na clique máxima
+
+// Função para verificar se um vértice pode ser adicionado à clique atual
+bool isSafe(int v, int clique[], int size) {
+    for (int i = 0; i < size; i++) {
+        if (graph[v][clique[i]])
+            return false;
+    }
+    return true;
+}
+
+// Função recursiva para encontrar a clique máxima
+void findMaxClique(int clique[], int size, int v) {
+    if (v == numVertices) {
+        if (size > maxCliqueSize) {
+            maxCliqueSize = size;
+            #pragma omp critical
+            {
+                // Atualizar a clique máxima encontrada
+                for (int i = 0; i < size; i++) {
+                    clique[i] = clique[i];
+                }
+            }
+        }
+        return;
+    }
+
+    // Verifique se o vértice pode ser adicionado à clique
+    if (isSafe(v, clique, size)) {
+        // Inclua o vértice na clique e explore recursivamente
+        clique[size] = v;
+        findMaxClique(clique, size + 1, v + 1);
+    }
+
+    // Explore recursivamente sem incluir o vértice na clique
+    findMaxClique(clique, size, v + 1);
+}
+
+int main() {
+    // Inicialize seu grafo e outras variáveis aqui
+
+    // Inicialize o número de threads para OpenMP
+    int numThreads = 4; // Você pode ajustar isso de acordo com o número de núcleos do seu processador
+
+    omp_set_num_threads(numThreads);
+
+    maxCliqueSize = 0;
+
+    // Crie uma seção paralela para a busca da clique máxima
+    #pragma omp parallel
+    {
+        int thread_id = omp_get_thread_num();
+        int clique_private[MAX_VERTICES];
+        #pragma omp for
+        for (int i = 0; i < numVertices; i++) {
+            // Cada thread começa a busca da clique a partir de um vértice diferente
+            findMaxClique(clique_private, 0, i);
+        }
+    }
+
+    // O máximo clique encontrado estará em 'clique' e seu tamanho em 'maxCliqueSize'
+
+    // Imprima o resultado aqui
+
+    return 0;
+}
--- paralelizcao.cpp
+++
@@ -1,5 +0,0 @@
-// Paralelização e Distribuição do Processamento com MPI
-// Nesta implementação, você deve dividir o problema em várias partes e distribuí-las para diferentes processadores usando a 
-// biblioteca MPI (Message Passing Interface). Cada processador será responsável por encontrar cliques em uma parte do grafo, e 
-// os resultados serão combinados no final para encontrar todas as cliques no grafo.
-//solucao aqui 
