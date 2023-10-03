aa9cebbc182c078e9b424d14aca6fd3609297c23
gabri190 <gabriel_avioes@hotmail.com>
2023-10-03 11:26:21 -0300

    adicionando outras implementacoes

---
+++ paralelizacao.cpp
@@ -0,0 +1,5 @@
+// Paralelização e Distribuição do Processamento com MPI
+// Nesta implementação, você deve dividir o problema em várias partes e distribuí-las para diferentes processadores usando a 
+// biblioteca MPI (Message Passing Interface). Cada processador será responsável por encontrar cliques em uma parte do grafo, e 
+// os resultados serão combinados no final para encontrar todas as cliques no grafo.
+//solucao aqui 
--- threads.cpp
+++ threads.cpp
@@ -8,3 +8,79 @@
 // mesmo assim você não vai conseguir resolver exaustivamente o problema, mas o tamanho do grafo será maior, e o tempo de 
 // processamento para um mesmo tamanho deve ser preferencialmente menor.
 //solucao aqui
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <mpi.h>
+
+#define MAX_VERTICES 1000
+
+int graph[MAX_VERTICES][MAX_VERTICES]; // Matriz de adjacência
+int numVertices; // Número de vértices no grafo
+int maxCliqueSize; // Tamanho máximo da clique
+int clique[MAX_VERTICES]; // Conjunto de vértices na clique máxima
+
+// Função para verificar se um vértice pode ser adicionado à clique atual
+bool isSafe(int v, int clique[], int size) {
+    for (int i = 0; i < size; i++) {
+        if (graph[v][clique[i]])
+            return false;
+    }
+    return true;
+}
+
+// Função recursiva para encontrar a clique máxima em uma parte do grafo
+void findMaxClique(int clique[], int size, int v) {
+    if (v == numVertices) {
+        if (size > maxCliqueSize) {
+            maxCliqueSize = size;
+            // Atualizar a clique máxima encontrada
+            for (int i = 0; i < size; i++) {
+                clique[i] = clique[i];
+            }
+        }
+        return;
+    }
+
+    // Verifique se o vértice pode ser adicionado à clique
+    if (isSafe(v, clique, size)) {
+        // Inclua o vértice na clique e explore recursivamente
+        clique[size] = v;
+        findMaxClique(clique, size + 1, v + 1);
+    }
+
+    // Explore recursivamente sem incluir o vértice na clique
+    findMaxClique(clique, size, v + 1);
+}
+
+int main(int argc, char *argv[]) {
+    // Inicialize o ambiente MPI
+    MPI_Init(&argc, &argv);
+
+    int rank, size;
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+
+    // Distribua o trabalho entre os processadores aqui
+    int verticesPerProcessor = numVertices / size;
+    int startVertex = rank * verticesPerProcessor;
+    int endVertex = (rank == size - 1) ? numVertices : startVertex + verticesPerProcessor;
+
+    maxCliqueSize = 0;
+
+    // Cada processador encontra a clique máxima em sua parte do grafo
+    int clique_private[MAX_VERTICES];
+    for (int i = startVertex; i < endVertex; i++) {
+        findMaxClique(clique_private, 0, i);
+    }
+
+    // Combina os resultados para encontrar a clique máxima global
+    // Use funções MPI para fazer isso
+
+    // Imprima o resultado aqui
+
+    // Finalize o ambiente MPI
+    MPI_Finalize();
+
+    return 0;
+}
