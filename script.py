da30888980834ece2f10ca641331bd2b302bc032
Gabriel De Araújo Alves <72319195+gabri190@users.noreply.github.com>
2023-11-23 15:58:46 -0300

    Update script.py

--- script.py
+++ script.py
@@ -1,52 +1,75 @@
 import matplotlib.pyplot as plt
 import subprocess
 import time
+import plotly.graph_objs as go
+from plotly.subplots import make_subplots
 
 def run_program(program_path, input_size):
+    # Modifique o caminho do programa para incluir o tamanho da entrada
+    specific_program_path = f"{program_path}{input_size}"
+    
     start_time = time.time()
-    result = subprocess.run([program_path, str(input_size)], capture_output=True, text=True)
+    result = subprocess.run([specific_program_path], capture_output=True, text=True)
     end_time = time.time()
     execution_time = end_time - start_time
+    
+    # Aqui você pode querer verificar se result.returncode == 0 para ter certeza de que o programa foi executado com sucesso
+    
     return execution_time
 
-# Caminhos para os executáveis
-serial_program_path = './exaustiva'
-heuristic_1_program_path = './threads'
-# heuristic_2_program_path = './seu_programa_heuristica_2'
+# Caminhos base para os executáveis
+serial_program_base_path = './exaustiva'
+heuristic_1_program_base_path = './threads'
 
-# Tamanhos de entrada
-input_sizes = [5, 10, 50, 100]
+# Tamanhos de entrada correspondem aos sufixos dos nomes dos executáveis
+input_sizes = [5, 10, 15, 20, 25]
 
 # Executar os programas e obter os tempos de execução
 tempo_serial = []
 tempo_heuristica_1 = []
 # tempo_heuristica_2 = []
 
+
 for size in input_sizes:
-    tempo_serial.append(run_program(serial_program_path, size))
-    tempo_heuristica_1.append(run_program(heuristic_1_program_path, size))
+    tempo_serial.append(run_program(serial_program_base_path, size))
+    tempo_heuristica_1.append(run_program(heuristic_1_program_base_path, size))
     # tempo_heuristica_2.append(run_program(heuristic_2_program_path, size))
 
 # Calcular speedups
 speedup_1 = [tempo_serial[i] / tempo_heuristica_1[i] for i in range(len(input_sizes))]
 # speedup_2 = [tempo_serial[i] / tempo_heuristica_2[i] for i in range(len(input_sizes))]
 
-# Criar gráficos
-fig, ax1 = plt.subplots()
+# Criar figura com subplots
+fig = make_subplots(rows=1, cols=1, specs=[[{'secondary_y': True}]])
 
-ax1.bar(input_sizes, tempo_serial, color='blue', label='Serial')
-ax1.bar(input_sizes, tempo_heuristica_1, color='green', label='Heurística 1')
-# ax1.bar(input_sizes, tempo_heuristica_2, color='orange', label='Heurística 2')
+# Adicionar gráfico de barras para tempos de execução
+fig.add_trace(
+    go.Bar(x=input_sizes, y=tempo_serial, name='Serial', marker=dict(color='blue')),
+    secondary_y=False,
+)
 
-ax2 = ax1.twinx()
-ax2.plot(input_sizes, speedup_1, color='red', linestyle='dashed', marker='o', label='Speedup (Heurística 1)')
-# ax2.plot(input_sizes, speedup_2, color='purple', linestyle='dashed', marker='o', label='Speedup (Heurística 2)')
+fig.add_trace(
+    go.Bar(x=input_sizes, y=tempo_heuristica_1, name='Heurística 1 (OpenMP)', marker=dict(color='green')),
+    secondary_y=False,
+)
 
-ax1.set_xlabel('Tamanho da Entrada')
-ax1.set_ylabel('Tempo de Execução (s)', color='black')
-ax2.set_ylabel('Speedup', color='black')
-ax1.legend(loc='upper left')
-ax2.legend(loc='upper right')
+# Adicionar gráfico de linha para speedup
+fig.add_trace(
+    go.Scatter(x=input_sizes, y=speedup_1, name='Speedup (OpenMP)', mode='lines+markers', line=dict(color='red', dash='dash')),
+    secondary_y=True,
+)
 
-plt.title('Comparação de Tempo de Execução e Speedup')
-plt.show()
+# Configurações do eixo x
+fig.update_xaxes(title_text='Tamanho da Entrada')
+
+# Configurações do eixo y primário
+fig.update_yaxes(title_text='Tempo de Execução (s)', secondary_y=False)
+
+# Configurações do eixo y secundário
+fig.update_yaxes(title_text='Speedup', secondary_y=True)
+
+# Atualizar layout da figura
+fig.update_layout(title='Comparação de Tempo de Execução e Speedup', barmode='group')
+
+# Mostrar figura
+fig.show()
